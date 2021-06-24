ef9831918260b39413e588bc018a72172306cd28
Tom Hermann <tom.hermann@wwt.com>
2021-06-24 09:46:43 -0500

    Created Java 11 version of the application, so folks without Java 16 can test time too!

--- src/main/java/com/wwt/testing/time/Person.java
+++ src/main/java/com/wwt/testing/time/Person.java
@@ -1,9 +1,44 @@
 package com.wwt.testing.time;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public record Person(
-    String name,
-    LocalDate birthday
-) {
+public final class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public LocalDate birthday() {
+        return birthday;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Person) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.birthday, that.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "Person[" +
+                "name=" + name + ", " +
+                "birthday=" + birthday + ']';
+    }
+
 }
--- src/main/java/com/wwt/testing/time/notifications/Notification.java
+++ src/main/java/com/wwt/testing/time/notifications/Notification.java
@@ -1,9 +1,45 @@
 package com.wwt.testing.time.notifications;
 
-public record Notification(
-    String title,
-    String message
-){
+import java.util.Objects;
+
+public class Notification {
+    private final String title;
+    private final String message;
+
+    public Notification(String title, String message) {
+        this.title = title;
+        this.message = message;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String message() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Notification) obj;
+        return Objects.equals(this.title, that.title) &&
+                Objects.equals(this.message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, message);
+    }
+
+    @Override
+    public String toString() {
+        return "Notification[" +
+                "title=" + title + ", " +
+                "message=" + message + ']';
+    }
+
     public static Notification of(String title, String message) {
         return new Notification(title, message);
     }
