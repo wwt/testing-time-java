cd33b13929cff42776f79c9d88a56cdd9472c190
Tom Hermann <tom.hermann@wwt.com>
2021-12-17 08:49:55 -0600

    Remove log4j.

--- src/test/java/com/wwt/testing/time/notifications/BirthdayNotificationGeneratorMockTest.java
+++ src/test/java/com/wwt/testing/time/notifications/BirthdayNotificationGeneratorMockTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class BirthdayNotificationGeneratorMockTest {
+class BirthdayNotificationGeneratorMockTest {
     @Mock
     private Clock clock;
     @InjectMocks
